252096fd53c03418b265b9698051e429f3cad921
Sean Zellmer <sean@lejeunerenard.com>
2013-09-10 22:10:02 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+DBIx::Class::ResultSet::Excel - Blah blah blah
+
+# SYNOPSIS
+
+    use DBIx::Class::ResultSet::Excel;
+
+# DESCRIPTION
+
+DBIx::Class::ResultSet::Excel is
+
+# AUTHOR
+
+Sean Zellmer <sean@lejeunerenard.com>
+
+# COPYRIGHT
+
+Copyright 2013- Sean Zellmer
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/DBIx/Class/ResultSet/Excel.pm
@@ -0,0 +1,39 @@
+package DBIx::Class::ResultSet::Excel;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Class::ResultSet::Excel - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use DBIx::Class::ResultSet::Excel;
+
+=head1 DESCRIPTION
+
+DBIx::Class::ResultSet::Excel is
+
+=head1 AUTHOR
+
+Sean Zellmer E<lt>sean@lejeunerenard.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Sean Zellmer
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
